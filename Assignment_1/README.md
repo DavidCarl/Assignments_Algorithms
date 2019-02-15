518c2dd69c20ce698d77ff20622b78902c878ff5
Alexander A. Nielsen <cph-an178@cphbusiness.dk>
2019-02-15 03:48:21 +0100

    Added readme and summary

---
+++ Assignment_1/README.md
@@ -0,0 +1,25 @@
+# Assignment 1 - Sorting Shakespeare’s complete works
+
+This is the first assignment for the Data structure and Algorithms course.
+It reads Shakespeare's completed works as a text file, then sorts the file.
+
+## Features
+
+- Read any textfile (as the above into an array or ArrayList)
+  - All words should be lower case
+  - No punktuations - strip commas, full stops, etc.
+- Sort the array using different sorting algorithms:
+  - Selection Sort
+  - Insertion Sort
+  - Merge sort  
+- Be extended with other sorting algorithms in the future. 
+
+## How to run
+
+1. Open the project in an IDE that can run Java
+2. Change file path in FileManager.java to your path to the dataset
+3. Run the main file
+
+**OBS:** It take approximately # hours to run this program on the full dataset.
+We advise you use `shakespear_s.txt` text file as it only takes a couple of minutes 
+to run.
--- Assignment_1/src/main/java/main.java
+++ Assignment_1/src/main/java/main.java
@@ -3,20 +3,20 @@ import java.util.List;
 public class main {
     public static void main(String[] args) {
         // Run code here!
-        //FileManager fm = new FileManager("shakespear_s.txt");
+        FileManager fm = new FileManager("/home/alex/Projects/Assignments_Algorithms/Assignment_1/src/main/java/shakespear_l.txt");
         SortingAlgoritms SA = new SortingAlgoritms();
 
-        //String[] words = fm.getWordsAsArray();
-        String[] words = {"zee", "bee", "ahhh", "aahh", "aaah", "ahh", "cee", "xee", "hee"}; //Test array!
+        String[] words = fm.getWordsAsArray();
+        //String[] words = {"zee", "bee", "ahhh", "aahh", "aaah", "ahh", "cee", "xee", "hee"}; //Test array!
         System.out.println("Amount of words: " + words.length);
 
         long selectionStart = System.currentTimeMillis();
-        String[] sortedWords = SA.selectionSort(words);
+        //String[] sortedWords = SA.selectionSort(words);
         long selectionEnd = System.currentTimeMillis();
         System.out.println("selectionSort timing: " + (selectionEnd - selectionStart));
 
         long insertionStart = System.currentTimeMillis();
-        String[] insertionWords = SA.insertionSort(words);
+        //String[] insertionWords = SA.insertionSort(words);
         long insertionEnd = System.currentTimeMillis();
         System.out.println("insertionSort timing: " + (insertionEnd - insertionStart));
 
