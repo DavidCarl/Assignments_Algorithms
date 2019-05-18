b960faf28e362fd01f87d16f3933923281f46c48
David <david.martin.carl@gmail.com>
2019-05-18 14:46:50 +0200

    File splitter

--- Assignment-5/src/main/java/main/Main.java
+++ Assignment-5/src/main/java/main/Main.java
@@ -2,15 +2,24 @@ package main;
 
 import utils.FileGenerator;
 import utils.Stopwatch;
+import utils.Reader;
+
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
         FileGenerator fg = new FileGenerator();
+        Reader reader = new Reader();
 
-        try (Stopwatch s = new Stopwatch()) {
-            fg.generateFile(10);
+//        try (Stopwatch s = new Stopwatch()) {
+//            fg.generateFile(10);
+//        }
+
+        try {
+            reader.splitFile("Files/Generated/GeneratedFile.txt", 64);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 }
---
+++ Assignment-5/src/main/java/utils/Reader.java
@@ -0,0 +1,59 @@
+package utils;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Reader {
+
+    //USING THIS SPLITTER http://www.admios.com/blog/how-to-split-a-file-using-java VERSION 3, CHANNELING
+
+    private final String dir = "Files/split/";
+    private final String suffix = ".splitPart";
+
+    public List<Path> splitFile(final String fileName, final int mBperSplit) throws IOException {
+
+        if (mBperSplit <= 0) {
+            throw new IllegalArgumentException("mBperSplit must be more than zero");
+        }
+
+        List<Path> partFiles = new ArrayList<>();
+        final long sourceSize = Files.size(Paths.get(fileName));
+        final long bytesPerSplit = 1024L * 1024L * mBperSplit;
+        final long numSplits = sourceSize / bytesPerSplit;
+        final long remainingBytes = sourceSize % bytesPerSplit;
+        int position = 0;
+
+        try (RandomAccessFile sourceFile = new RandomAccessFile(fileName, "r");
+             FileChannel sourceChannel = sourceFile.getChannel()) {
+
+            for (; position < numSplits; position++) {
+                //write multipart files.
+                writePartToFile(bytesPerSplit, position * bytesPerSplit, sourceChannel, partFiles);
+            }
+
+            if (remainingBytes > 0) {
+                writePartToFile(remainingBytes, position * bytesPerSplit, sourceChannel, partFiles);
+            }
+        }
+        return partFiles;
+    }
+
+    private void writePartToFile(long byteSize, long position, FileChannel sourceChannel, List<Path> partFiles) throws IOException {
+        Path fileName = Paths.get(dir + UUID.randomUUID() + suffix);
+        try (RandomAccessFile toFile = new RandomAccessFile(fileName.toFile(), "rw");
+             FileChannel toChannel = toFile.getChannel()) {
+            sourceChannel.position(position);
+            toChannel.transferFrom(sourceChannel, 0, byteSize);
+        }
+        partFiles.add(fileName);
+    }
+
+
+}
