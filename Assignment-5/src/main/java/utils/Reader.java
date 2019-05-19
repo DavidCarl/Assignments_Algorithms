72fedf19cf2e96c3f85d2d668bf86a98d85b7323
David <david.martin.carl@gmail.com>
2019-05-19 15:20:51 +0200

    Correct output size

--- Assignment-5/src/main/java/utils/Reader.java
+++ Assignment-5/src/main/java/utils/Reader.java
@@ -36,25 +36,20 @@ public class Reader {
         try (RandomAccessFile sourceFile = new RandomAccessFile(fileName, "r");
              FileChannel sourceChannel = sourceFile.getChannel()) {
             for (; position < numSplits; position++) {
+                System.out.println(position);
                 for(long i = ((position*bytesPerSplit) + bytesPerSplit); i > 0; i--){
                     sourceFile.seek(i);
                     if(sourceFile.readByte() == 48) {
                         start = end;
                         end = i;
-                        System.out.println((end - start) + " : " + end);
+                        System.out.println("Start: " + start + " \t--> " + end);
                         writePartToFile((end - start), start, sourceChannel, partFiles);
                         break;
                     }
                 }
-//                System.out.println("BYTE START!");
-//                System.out.println(start);
-//                System.out.println("BYTE END! FIRST I DIRECT AND SECOND IS NEWLINE SPLIT");
-//                System.out.println((position*bytesPerSplit) + bytesPerSplit);
-//                System.out.println(end);
-//                System.out.println("");
             }
             if (remainingBytes > 0) {
-                System.out.println(sourceSize + " REST FILE! " + (sourceSize - end));
+                System.out.println("Start: " + end + " \t--> " + sourceSize);
                 writePartToFile((sourceSize - end), end, sourceChannel, partFiles);
             }
         }
@@ -63,7 +58,7 @@ public class Reader {
 
     private void writePartToFile(long byteSize, long position, FileChannel sourceChannel, List<Path> partFiles) throws IOException {
         Path fileName = Paths.get(dir + UUID.randomUUID() + suffix);
-//        System.out.println(fileName.toFile());
+        System.out.println(fileName.toFile());
         try (RandomAccessFile toFile = new RandomAccessFile(fileName.toFile(), "rw");
              FileChannel toChannel = toFile.getChannel()) {
             sourceChannel.position(position);
